7770a6f07538417ec9580cddcfb811bf42d70be5
Stephen Panaro <stephen.panaro@gmail.com>
2015-12-28 21:22:26 -0500

    Add simple websocket server

--- light-controller/light-controller.ino
+++ light-controller/light-controller.ino
@@ -8,13 +8,21 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <WebSocketsServer.h>
 
+// Constants
 char ssid[] = "ssid";
 char pass[] = "pass";  
 char mdns_name[] = "steves_esp";
 
+WiFiServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+// Forward declarations
 void connectToWifi();
 void advertiseMdns();
+void setupWebsockets();
+void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
 
 void setup() {
   Serial.begin(115200);
@@ -24,16 +32,60 @@ void setup() {
 
   connectToWifi();
   advertiseMdns();
+  setupWebsockets();
 }
 
 void loop() {
+  webSocket.loop();
 
   delay(1000);
+}
+
+void setupWebsockets() {
+  Serial.println("Setting up websockets");
+  webSocket.begin();
+  webSocket.onEvent(handleWebSocketEvent);
+  Serial.println("Set up.");
+}
+
+void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.println("Disconnected!");
+            break;
+        case WStype_CONNECTED: {
+            IPAddress ip = webSocket.remoteIP(num);
+            Serial.println("Connected");
+
+            // send message to client
+            webSocket.sendTXT(num, "Connected");
+        }
+            break;
+        case WStype_TEXT:
+            Serial.println("Get text");
+
+            // send message to client
+            // webSocket.sendTXT(num, "message here");
+
+            // send data to all connected clients
+            // webSocket.broadcastTXT("message here");
+            break;
+        case WStype_BIN:
+            Serial.println("get bin");
+
+            // send message to client
+            // webSocket.sendBIN(num, payload, lenght);
+            break;
+    }
 }
 
 void connectToWifi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  
   WiFi.begin(ssid, pass);
   
   while (WiFi.status() != WL_CONNECTED) {
