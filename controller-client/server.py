783121c14553631e337ba440bf7de6915523cc0a
Stephen Panaro <stephen.panaro@gmail.com>
2016-01-02 00:58:29 -0500

    Add HSB

--- controller-client/server.py
+++ controller-client/server.py
@@ -1,20 +1,28 @@
 from bottle import route, run, template, request, response, hook
 import json
-from colors import Color, NUM_LEDS, safely_send_colors
+import colorsys
 
-current_color = {
-  "h":0,
-  "s":0,
-  "b":0
-}
-current_state = OFF_STATE
+from colors import Color, NUM_LEDS, safely_send_colors
 
 ON_STATE = "on"
 OFF_STATE = "off"
 VALID_STATES = [ON_STATE, OFF_STATE]
 STATE_KEY = "state"
 
-# Called from the website to submit a new lighting configuration.
+HUE_KEY = "hue"
+SATURATION_KEY = "saturation"
+BRIGHTNESS_KEY = "brightness"
+
+HUE_INDEX = 0
+SATURATION_INDEX = 1
+BRIGHTNESS_INDEX = 2
+
+# Lights state
+current_color_hsb = [0,0,0]
+current_state = OFF_STATE
+
+# On/Off state
+
 @route("/state", method=["POST"])
 def index():
   global current_state
@@ -37,7 +45,6 @@ def index():
   else:
     response.status = 400
 
-# Called from the light_client to get the current configuration.
 @route("/state")
 def index():
   global current_state
@@ -45,13 +52,102 @@ def index():
   response.status = 200
   return json.dumps({STATE_KEY: current_state})
 
+# Hue
+@route("/hue", method=["POST"])
+def index():
+  global current_state
+
+  params = request.json
+  if not params:
+    response.status = 400
+    return
+
+  new_hue = params.get(HUE_KEY, None)
+  if new_hue >= 0 and new_hue <= 360:
+    current_color_hsb[HUE_INDEX] = new_hue
+    display_current_color()
+
+    response.status = 200
+  else:
+    response.status = 400
+
+@route("/hue")
+def index():
+  global current_state
+  response.headers["Content-type"] = "application/json"
+  response.status = 200
+  return json.dumps({HUE_KEY: current_color_hsb[HUE_INDEX]})
+
+# Saturation
+@route("/saturation", method=["POST"])
+def index():
+  global current_state
+
+  params = request.json
+  if not params:
+    response.status = 400
+    return
+
+  new_saturation = params.get(SATURATION_KEY, None)
+  if new_saturation >= 0 and new_saturation <= 360:
+    current_color_hsb[SATURATION_INDEX] = new_saturation
+    display_current_color()
+
+    response.status = 200
+  else:
+    response.status = 400
+
+@route("/saturation")
+def index():
+  global current_state
+  response.headers["Content-type"] = "application/json"
+  response.status = 200
+  return json.dumps({SATURATION_KEY: current_color_hsb[SATURATION_INDEX]})
+
+# Saturation
+@route("/brightness", method=["POST"])
+def index():
+  global current_state
+
+  params = request.json
+  if not params:
+    response.status = 400
+    return
+
+  new_brightness = params.get(BRIGHTNESS_KEY, None)
+  if new_brightness >= 0 and new_brightness <= 360:
+    current_color_hsb[BRIGHTNESS_INDEX] = new_brightness
+    display_current_color()
+
+    response.status = 200
+  else:
+    response.status = 400
+
+@route("/brightness")
+def index():
+  global current_state
+  response.headers["Content-type"] = "application/json"
+  response.status = 200
+  return json.dumps({BRIGHTNESS_KEY: current_color_hsb[BRIGHTNESS_INDEX]})
+
 # light helpers
+
+def get_current_color_rgb():
+  hsb_percents = (current_color_hsb[0]/360.0, current_color_hsb[1]/100.0, current_color_hsb[2]/100.0)
+  rgb_percents = colorsys.hsv_to_rgb(*hsb_percents)
+  rgb_values = tuple([int(c*254) for c in rgb_percents]) # 0 - 254
+  return Color(*rgb_values)
 
 def turn_off():
   display_single_color(Color(0,0,0))
 
 def display_current_color():
-  display_single_color(Color(30,0,0))
+  c = get_current_color_rgb()
+  MAX_RGB = 30 # Don't burn out the power supply.
+  c.r = min(c.r, MAX_RGB)
+  c.g = min(c.g, MAX_RGB)
+  c.b = min(c.b, MAX_RGB)
+  display_single_color(c)
 
 def display_single_color(color):
   colors = [color] * NUM_LEDS
--- homebridge-simple-ledstrip/index.js
+++ homebridge-simple-ledstrip/index.js
@@ -22,21 +22,23 @@ function LedStripAccessory(log, config) {
     .on('get', this.getState.bind(this))
     .on('set', this.setState.bind(this));
 
-  // this.service
-  //   .getCharacteristic(Characteristic.Hue)
-  //   .on('get', this.getState.bind(this))
-  //   .on('set', this.setState.bind(this));
+  this.service
+    .getCharacteristic(Characteristic.Hue)
+    .on('get', this.getHue.bind(this))
+    .on('set', this.setHue.bind(this));
 
-  // this.service
-  //   .getCharacteristic(Characteristic.Hue)
-  //   .on('get', this.getState.bind(this))
-  //   .on('set', this.setState.bind(this));
+  this.service
+    .getCharacteristic(Characteristic.Saturation)
+    .on('get', this.getSaturation.bind(this))
+    .on('set', this.setSaturation.bind(this));
 
-  // this.service
-  //   .getCharacteristic(Characteristic.Hue)
-  //   .on('get', this.getState.bind(this))
-  //   .on('set', this.setState.bind(this));
+  this.service
+    .getCharacteristic(Characteristic.Brightness)
+    .on('get', this.getBrightness.bind(this))
+    .on('set', this.setBrightness.bind(this));
 }
+
+// Helpers
 
 // endpoint: without a leading or trailing slash e.g. 'state', 'hue', 'saturation' ...
 // params: map of query params
@@ -52,6 +54,8 @@ LedStripAccessory.prototype.makeGet = function(endpoint, params, callback) {
 LedStripAccessory.prototype.makePost = function(endpoint, body, callback) {
   return request.post({url: this.getURL(endpoint), endpoint, body: body, json: true}, callback);
   }
+
+// On/Off State
 
 LedStripAccessory.prototype.getState = function(callback) {
   this.log("Getting current state...");
@@ -88,6 +92,128 @@ LedStripAccessory.prototype.setState = function(state, callback) {
     else {
       this.log("Error '%s' setting light state. Response: %s", err, body);
       callback(err || new Error("Error setting light state."));
+    }
+  }.bind(this));
+}
+
+// Hue
+
+LedStripAccessory.prototype.getHue = function(callback) {
+  this.log("Getting current hue...");
+
+  this.makeGet('hue', {}, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      var response = JSON.parse(body);
+      var hue = response.hue;
+      this.log("Strip hue is %s", hue);
+      callback(null, hue); // success
+    }
+    else {
+      this.log("Error: %s", err);
+      this.log("Response: %s", response);
+      this.log("Error getting hue (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
+}
+
+LedStripAccessory.prototype.setHue = function(hue, callback) {
+  this.log("Set hue to %s", hue);
+
+  this.makePost('hue', {hue: hue}, function(err, response, body) {
+
+    if (!err && response.statusCode == 200) {
+      this.log("hue change complete.");
+
+      callback(null); // success
+    }
+    else {
+      this.log("Error '%s' setting light hue. Response: %s", err, body);
+      callback(err || new Error("Error setting light hue."));
+    }
+  }.bind(this));
+}
+
+LedStripAccessory.prototype.getServices = function() {
+  return [this.service];
+}
+
+// Saturation
+
+LedStripAccessory.prototype.getSaturation = function(callback) {
+  this.log("Getting current saturation...");
+
+  this.makeGet('saturation', {}, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      var response = JSON.parse(body);
+      var saturation = response.saturation;
+      this.log("Strip saturation is %s", saturation);
+      callback(null, saturation); // success
+    }
+    else {
+      this.log("Error: %s", err);
+      this.log("Response: %s", response);
+      this.log("Error getting saturation (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
+}
+
+LedStripAccessory.prototype.setSaturation = function(saturation, callback) {
+  this.log("Set saturation to %s", saturation);
+
+  this.makePost('saturation', {saturation: saturation}, function(err, response, body) {
+
+    if (!err && response.statusCode == 200) {
+      this.log("saturation change complete.");
+
+      callback(null); // success
+    }
+    else {
+      this.log("Error '%s' setting light saturation. Response: %s", err, body);
+      callback(err || new Error("Error setting light saturation."));
+    }
+  }.bind(this));
+}
+
+LedStripAccessory.prototype.getServices = function() {
+  return [this.service];
+}
+
+// Brightness
+
+LedStripAccessory.prototype.getBrightness = function(callback) {
+  this.log("Getting current brightness...");
+
+  this.makeGet('brightness', {}, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      var response = JSON.parse(body);
+      var brightness = response.brightness;
+      this.log("Strip brightness is %s", brightness);
+      callback(null, brightness); // success
+    }
+    else {
+      this.log("Error: %s", err);
+      this.log("Response: %s", response);
+      this.log("Error getting hue (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
+}
+
+LedStripAccessory.prototype.setBrightness = function(brightness, callback) {
+  this.log("Set brightness to %s", brightness);
+
+  this.makePost('brightness', {brightness: brightness}, function(err, response, body) {
+
+    if (!err && response.statusCode == 200) {
+      this.log("brightness change complete.");
+
+      callback(null); // success
+    }
+    else {
+      this.log("Error '%s' setting light brightness. Response: %s", err, body);
+      callback(err || new Error("Error setting light brightness."));
     }
   }.bind(this));
 }
